3ad466982ff94e3d0528ec114c7c1198445f98f3
stylish_code <minyoung0101q@gmail.com>
2023-12-29 16:14:19 +0900

    es5 function + this 사용 가능

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const minyoung = {
   name: "Minyoung",
   age: 24,
-  addYear: () => {
-    this.age++; // this가 window를 가리킴
+  addYear() {
+    this.age++; // this가 minyoung 자신을 가리킴
   },
 };
-console.log(minyoung); // minyoung 객체 로그 출력
+console.log(minyoung); //minyoung 객체 로그 출력
 minyoung.addYear();
 minyoung.addYear();
 console.log(minyoung);
