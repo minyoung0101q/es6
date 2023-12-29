75a0dc5490bc2bd08919820513e3e59558248ed1
stylish_code <minyoung0101q@gmail.com>
2023-12-29 15:59:04 +0900

    addEventlistener + arrow function = this 사용 못함

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("button");
-function handleClick() {
+const handleClick = () => {
   console.log(this.style);
   this.style.backgroundColor = "red";
-}
+};
 button.addEventListener("click", handleClick);
